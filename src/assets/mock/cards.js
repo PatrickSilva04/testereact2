48595dcc3ba2114b53a71e7dfff53b36ad17a484
Patrick Ferreira da Silva <99125354+PatrickSilva04@users.noreply.github.com>
2025-04-09 11:30:50 -0300

    Add files via upload

--- src/assets/mock/cards.js
+++ src/assets/mock/cards.js
@@ -1,22 +1,10 @@
-import photos from "../images/images1.jpg"
-export const cards =[
-    {
-        text: "primeiro", img: photo1
-    },
-    {
-        text: "segundo", img: photo1
-    },
-    {
-        text: "terceiro", img: photo1
-    },
-    {
-        text: "quanto", img: photo1
-    },
-    {
-        text: "quinto", img: photo1
-    },
-    {
-        text: "sexto", img: photo1
-    },
-];
+import img01 from '../images/images1.jpg';
 
+export const cards = [
+  { text: "primeiro", img: img01 },
+  { text: "segundo", img: img01 },
+  { text: "terceiro", img: img01 },
+  { text: "quarto", img: img01 },
+  { text: "quinto", img: img01 },
+  { text: "sexto", img: img01 }
+];
